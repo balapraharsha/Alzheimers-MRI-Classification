95e94c6809ce3c1fbb5b1c2230f91c03df6cc2b3
MANNEPALLI BALA PRAHARSHA <balapraharsha.m@gmail.com>
2025-10-07 10:16:03 +0530

    Update app.py

--- Python Executable Files/app/app.py
+++ Python Executable Files/app/app.py
@@ -80,5 +80,5 @@ if __name__ == "__main__":
     if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", debug=True)
 
